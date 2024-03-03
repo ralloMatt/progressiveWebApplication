947ff5de673a4ea6de77514ee9322bd14ea21407
Matt R <mattrallo2012@gmail.com>
2024-03-02 23:23:32 -0600

    idb works

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,45 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  
+  console.log("Post this to the database!");
+
+  const jateDB = await openDB('jate', 1); // create connection
+
+  const transaction = jateDB.transaction('jate', 'readwrite'); // create transaction and specify database
+
+  const store = transaction.objectStore('jate'); // open store
+
+  const request = store.add({ content }); 
+
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+
+// logic for a method that gets all the content from the database
+export const getDb = async () => {
+  
+  console.log('Get stuff from the database');
+
+  const jateDB = await openDB('jate', 1); // create connection
+
+  const transaction = jateDB.transaction('jate', 'readonly'); // create transaction and specify database
+
+  const store = transaction.objectStore('jate'); // open store
+
+  const request = store.getAll(); 
+
+  const result = await request;
+  console.log('result.value', result);
+}; 
+
+/* // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
+ */
 
 initdb();
